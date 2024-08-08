cc61c1cf589b2fb90d462cdfdf13c70483069e44
Mark Fisher <mark.j.fisher@gmail.com>
2024-08-08 15:40:58 +0100

    [c64] better disk creation

--- makefiles/custom-c64.mk
+++ makefiles/custom-c64.mk
@@ -9,11 +9,17 @@ DISK_TASKS += .create-d64
 DISK_FILE = $(DIST_DIR)/$(PROGRAM).d64
 
 .create-d64:
-	@echo "creating $(DISK_FILE) from program $(PROGRAM_TGT)"
-	@if [ -f $(DISK_FILE) ] ; then \
-	  rm $(DISK_FILE) ; \
-	fi ;
-	@cc1541 -q -n "fle: $(PROGRAM)" -f "$(PROGRAM)" -P -w $(DIST_DIR)/$(PROGRAM_TGT)$(SUFFIX) -H "created by fenrock" $(DISK_FILE)
+	@which cc1541 > /dev/null 2>&1 ; \
+	if [ $$? -eq 0 ] ; then \
+		echo "creating $(DISK_FILE) from program $(PROGRAM_TGT)" ; \
+		if [ -f "$(DISK_FILE)" ] ; then \
+			rm "$(DISK_FILE)" ; \
+		fi ; \
+		cc1541 -q -n "fn-lib: $(PROGRAM)" -f "$(PROGRAM)" -P -w $(DIST_DIR)/$(PROGRAM_TGT)$(SUFFIX) -H "created by fenrock" $(DISK_FILE) ; \
+	else \
+		echo -e "\nERROR! You must install cc1541 from https://bitbucket.org/ptv_claus/cc1541/src/master/ to create commodore disks\n" ; \
+		exit 1 ; \
+	fi
 
 ################################################################
 # TESTING / EMULATOR
